43d65b78404b4109197de334e96c309d9d3e4521
Shivam Vishwakarma <recxcoder@gmail.com>
2026-09-16 01:40:29 +0530

    feat: add Vercel URL to CORS

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-    "https://legalease-frontend.vercel.app",
-    "*"],
+    "https://legalease-umber-nu.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
