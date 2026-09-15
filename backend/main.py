00bdd7cf11171e96d8eec658f760e6ee070f6175
Shivam Vishwakarma <recxcoder@gmail.com>
2026-09-16 01:20:09 +0530

    feat: connect frontend to Render backend

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+    "https://legalease-frontend.vercel.app",
+    "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
