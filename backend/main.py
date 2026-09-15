81e3acafd3a02db27dbb729a645d86407e7f868a
Shivam Vishwakarma <recxcoder@gmail.com>
2026-09-16 02:00:35 +0530

    fix: update CORS for new domain getlegalease.vercel.app

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-    "https://legalease-umber-nu.vercel.app"],
+    "https://getlegalease.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
